e9ac5619ec9e0706392d00955234c4cf283fb9fe
Valentina Gorshkova <Anitnelav01@yandex.ru>
2026-03-07 00:27:38 +0300

    optimized images for adaptive

--- webpack.config.js
+++ webpack.config.js
@@ -1,134 +1,22 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
-
-const isProduction = process.env.NODE_ENV === "production";
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.[contenthash].js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-    publicPath: "/",
   },
-  mode: isProduction ? "production" : "development",
-  devtool: isProduction ? "source-map" : "eval-source-map",
+  mode: "development",
 
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              [
-                "@babel/preset-env",
-                {
-                  useBuiltIns: "usage",
-                  corejs: 3,
-                  targets: "> 0.25%, not dead",
-                },
-              ],
-            ],
-            cacheDirectory: true,
-          },
-        },
-      },
-      {
         test: /\.css$/,
-        use: [
-          isProduction ? MiniCssExtractPlugin.loader : "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-            },
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [
-                  ["autoprefixer"],
-                  ...(isProduction ? ["cssnano"] : []),
-                ],
-              },
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
-        oneOf: [
-          {
-            resourceQuery: /inline/,
-            type: "asset/inline",
-            parser: {
-              dataUrlCondition: {
-                maxSize: 8 * 1024, // 8kb
-              },
-            },
-          },
-          {
-            type: "asset",
-            parser: {
-              dataUrlCondition: {
-                maxSize: 4 * 1024, // 4kb
-              },
-            },
-            generator: {
-              filename: "assets/images/[name].[hash:8][ext]",
-            },
-            use: [
-              {
-                loader: "image-webpack-loader",
-                options: {
-                  disable: !isProduction,
-                  mozjpeg: {
-                    progressive: true,
-                    quality: 65,
-                  },
-                  optipng: {
-                    enabled: true,
-                    optimizationLevel: 5,
-                  },
-                  pngquant: {
-                    quality: [0.65, 0.9],
-                    speed: 4,
-                  },
-                  gifsicle: {
-                    interlaced: false,
-                  },
-                  webp: {
-                    quality: 60,
-                  },
-                },
-              },
-            ],
-          },
-        ],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "assets/fonts/[name].[hash:8][ext]",
-        },
-      },
-      {
-        test: /\.mp4$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "assets/videos/[name].[hash:8][ext]",
-        },
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
@@ -136,157 +24,25 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      filename: "index.html",
-      inject: "body",
-      scriptLoading: "defer",
-      minify: isProduction
-        ? {
-            removeComments: true,
-            collapseWhitespace: true,
-            removeRedundantAttributes: true,
-            useShortDoctype: true,
-            removeEmptyAttributes: true,
-            removeStyleLinkTypeAttributes: true,
-            keepClosingSlash: true,
-            minifyJS: true,
-            minifyCSS: true,
-            minifyURLs: true,
-          }
-        : false,
-      chunks: ["main"],
     }),
-
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
     new CopyPlugin({
       patterns: [
         {
           from: "src/assets/images",
           to: "assets/images",
-          globOptions: {
-            ignore: ["**/*.js", "**/*.css", "**/*.svg"],
-          },
-        },
-        {
-          from: "src/assets/videos",
-          to: "assets/videos",
-          noErrorOnMissing: true,
-        },
-        {
-          from: "src/assets/fonts",
-          to: "assets/fonts",
           noErrorOnMissing: true,
         },
       ],
     }),
-
-    ...(isProduction
-      ? [
-          new MiniCssExtractPlugin({
-            filename: "css/[name].[contenthash].css",
-          }),
-        ]
-      : []),
-
-    ...(process.env.ANALYZE ? [new BundleAnalyzerPlugin()] : []),
   ],
-
-  optimization: {
-    minimize: isProduction,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: isProduction,
-            drop_debugger: true,
-            pure_funcs: ["console.log", "console.info", "console.debug"],
-          },
-          format: {
-            comments: false,
-          },
-        },
-        extractComments: false,
-      }),
-      new CssMinimizerPlugin(),
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.imageminMinify,
-          options: {
-            plugins: [
-              ["gifsicle", { interlaced: true }],
-              ["jpegtran", { progressive: true }],
-              ["optipng", { optimizationLevel: 5 }],
-              ["pngquant", { quality: [0.65, 0.9], speed: 4 }],
-              [
-                "svgo",
-                {
-                  plugins: [
-                    {
-                      name: "preset-default",
-                      params: { overrides: { removeViewBox: false } },
-                    },
-                    { name: "removeComments", active: true },
-                    { name: "removeTitle", active: true },
-                  ],
-                },
-              ],
-            ],
-          },
-        },
-        generator: [
-          {
-            type: "asset",
-            implementation: ImageMinimizerPlugin.imageminGenerate,
-            options: {
-              plugins: ["imagemin-webp"],
-            },
-            filename: "assets/images/[name].[hash:8].webp",
-          },
-        ],
-      }),
-    ],
-    splitChunks: false,
-    runtimeChunk: false,
-  },
-
-  performance: {
-    hints: isProduction ? "warning" : false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
-  },
 
   devServer: {
     static: "./dist",
     port: 3000,
     open: true,
     hot: true,
-    liveReload: true,
-    historyApiFallback: true,
-    compress: true,
-    client: {
-      overlay: {
-        errors: true,
-        warnings: false,
-      },
-    },
-  },
-
-  resolve: {
-    extensions: [".js", ".json"],
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-
-  cache: {
-    type: "filesystem",
-    buildDependencies: {
-      config: [__filename],
-    },
-  },
-
-  stats: {
-    modules: false,
-    children: false,
-    chunks: false,
-    chunkModules: false,
   },
 };
